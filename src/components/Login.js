8691a68dc533292e5c96f34f930930c5412d278b
Pavithran2 <pavithrank02@gmail.com>
2023-08-30 18:25:18 +0530

    UI updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -80,14 +80,14 @@ const Login = () => {
       <Header />
       <div className='absolute'>
         <img
-          className='h-screen object-cover md:w-screen'
+          className='h-screen object-cover md:w-screen '
           src={BG_URL}
           alt='Netflix Logo'
         />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
-        className='w-3/4 md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+        className='w-3/4 sm:w-3/4 md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
         <h1 className='text-2xl font-bold md:text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         {!isSignInForm && <input ref={name} type='test' placeholder='Full Name' className='p-4 my-2 w-full bg-gray-700 rounded-lg' />}
         <input ref={email} type='text' placeholder='Email Address' className='p-4 my-2 w-full bg-gray-700 rounded-lg' />
