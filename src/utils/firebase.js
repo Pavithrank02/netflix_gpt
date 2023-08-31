3c34240e8fba15d4936209e413ba04948afdb977
Pavithran2 <pavithrank02@gmail.com>
2023-08-31 09:46:23 +0530

    hosting challenges

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,15 +8,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAsP-AwmCjP3NBe5PbgQkac9ClCM3rEeGM",
-  authDomain: "netflix-gpt-224c2.firebaseapp.com",
-  projectId: "netflix-gpt-224c2",
-  storageBucket: "netflix-gpt-224c2.appspot.com",
-  messagingSenderId: "795160664911",
-  appId: "1:795160664911:web:59a1f95000818b5c5b033a",
-  measurementId: "G-7LCV1YSVKR"
+  apiKey: "AIzaSyBYjInkPZGh4WU2Tts6d21_uy1KMfZQ2K4",
+  authDomain: "netflixgpt-be3fa.firebaseapp.com",
+  projectId: "netflixgpt-be3fa",
+  storageBucket: "netflixgpt-be3fa.appspot.com",
+  messagingSenderId: "366094645015",
+  appId: "1:366094645015:web:88642739cb04b552016952",
+  measurementId: "G-78XQP8ZREM"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
